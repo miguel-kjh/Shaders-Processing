9b46575daf22e85b9a592fb5bc5faf0d210892df
miguel-kjh <miguelangel.kjh@gmail.com>
2020-04-16 11:57:02 +0100

    move done, click done

--- ejemplo/ejemplo.pde
+++ ejemplo/ejemplo.pde
@@ -1,22 +1,14 @@
 PShader sh;
-boolean paintCircules;
-
-float[] v1,v2;
+boolean paintPoint;
+float[] vecX = {random(0.,.99), random(0.,.99),random(0.,.99), random(0.,.99)};
+float[] vecY = {random(0.,.99), random(0.,.99),random(0.,.99), random(0.,.99)};
 
 void setup(){
   size(640,640,P3D);
-  sh            = loadShader("shader.glsl");
-  v1            = new float[2];
-  v1[0]         = random(0.,.99);
-  v1[1]         = random(0.,.99);
-  
-  v2            = new float[2];
-  v2[0]         = random(0.,.99);
-  v2[1]         = random(0.,.99);
-  println(v1);
-  println(v2);
-  sh.set("u_pos1",v1[0], v1[1]);
-  sh.set("u_pos2",v2[0], v2[1]);
+  sh = loadShader("shader.glsl");
+  paintPoint = false;
+  sh.set("u_vecX",vecX);
+  sh.set("u_vecY",vecY);
 }
 
 void draw(){
@@ -24,8 +16,16 @@ void draw(){
   shader(sh);
   sh.set("u_mouse",float(mouseX),float(mouseY));
   sh.set("u_resolution",float(width),float(height));
-  float toSeconds = 1000.0; 
+  float toSeconds = 3000.0; 
   sh.set("u_time",millis()/toSeconds);
+  sh.set("u_paintPoint",paintPoint);
   rect(0,0,width,height);
-  paintCircules = false;
+}
+
+void mousePressed(){
+  paintPoint = true;
+}
+
+void mouseReleased(){
+  paintPoint = false;
 }
--- ejemplo/shader.glsl
+++ ejemplo/shader.glsl
@@ -2,14 +2,19 @@
 precision mediump float;
 #endif
 
-#define RADIUS 0.0005
-#define NUM 1
+#define RADIUS    0.0005
+#define Thickness 0.003
+#define LEN       4
 
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
-uniform vec2 u_pos1;
-uniform vec2 u_pos2;
+uniform float u_vecX[LEN];
+uniform float u_vecY[LEN];
+uniform bool u_paintPoint;
+
+vec3 colorA = vec3(0.149,0.141,0.912);
+vec3 colorB = vec3(1.000,0.833,0.224);
 
 float random (vec2 st) {
     return fract(sin(dot(st.xy,
@@ -17,9 +22,21 @@ float random (vec2 st) {
         43758.5453123);
 }
 
-float plot(vec2 st, float pct){
-  return  smoothstep( pct-0.02, pct, st.y) -
-          smoothstep( pct, pct+0.02, st.y);
+vec2 calculePosition(vec2 pos, float speed){
+    vec2 result = pos + speed;
+    if(result.x < 0.0){
+        result.x = 0.0;
+    }
+    if(result.x > 0.99){
+        result.x = 0.99;
+    }
+    if(result.y < 0.0){
+        result.y = 0.0;
+    }
+    if(result.y > 0.99){
+        result.y = 0.99;
+    }
+    return result;
 }
 
 float circle(in vec2 _st, in float _radius){
@@ -30,13 +47,10 @@ float circle(in vec2 _st, in float _radius){
 }
 
 void drawCircle(vec2 pos){
-    //for(int index = 0; index < NUM; index++){
     vec3 color    = vec3(circle(pos,RADIUS));
-    gl_FragColor += vec4( color, 1.0 );
-    //}
+    gl_FragColor += vec4( color, 1.0 ) * vec4(colorB,1.0);
 } 
 
-#define Thickness 0.003
 
 float drawLine(vec2 p1, vec2 p2, vec2 st) {
 
@@ -48,16 +62,41 @@ float drawLine(vec2 p1, vec2 p2, vec2 st) {
 
   float p = (a + b + c) * 0.5;
 
-  // median to (p1, p2) vector
   float h = 2. / c * sqrt( p * ( p - a) * ( p - b) * ( p - c));
 
   return mix(1.0, 0.0, smoothstep(0.5 * Thickness, 1.5 * Thickness, h));
 }
 
+void drawGraph(vec2 st){
+    for(int index = 0; index < LEN; index++){
+        float speed = sin(u_time);
+        drawCircle(st - (calculePosition(vec2(u_vecX[index], u_vecY[index]), speed)));
+        for(int secondIndex = index + 1; secondIndex < LEN; secondIndex++){
+            gl_FragColor += drawLine(
+                calculePosition(vec2(u_vecX[index],       u_vecY[index]),       speed),
+                calculePosition(vec2(u_vecX[secondIndex], u_vecY[secondIndex]), speed),
+                st) * vec4(colorA,1.0);
+        }
+    }
+
+}
+
+void paintLastPoint(vec2 st){
+    float speed = sin(u_time);
+    vec2 mouse = u_mouse/u_resolution;
+    drawCircle(st - mouse);
+
+    for(int index = 0; index < LEN; index++){
+        gl_FragColor += drawLine(
+            mouse,
+            calculePosition(vec2(u_vecX[index], u_vecY[index]), speed),
+            st) * vec4(colorA,1.0);
+    }
+}
+
 void main() {
-    vec2 st   = gl_FragCoord.xy/u_resolution;
-    drawCircle(st - u_pos1);
-    drawCircle(st - u_pos2);
-    gl_FragColor += drawLine(u_pos1,u_pos2,st);
-    
+    //gl_FragColor += vec4(colorA,1.0);
+    vec2 st = gl_FragCoord.xy/u_resolution;
+    drawGraph(st);
+    if(u_paintPoint)paintLastPoint(st);
 }
